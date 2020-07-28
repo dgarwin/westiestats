f7ee1a1957c9cf8a63aadec22f64457d0f8259e1
David Garwin <davidgarwin@gmail.com>
2020-07-28 14:36:14 -0400

    WIP Audit these changes

--- analysis/basiStats.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-const DATA_DIR = 'data'
-async function main(){
-    const ret = {};
-    const data = JSON.parse(fs.readFileSync(`${DATA_DIR}/regPoints_full.json`));
-    console.log(Object.keys(ret).length);
-}
-main();
